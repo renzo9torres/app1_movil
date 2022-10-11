b58e54008fa0607e8f39dbbcda1d32cff20bab24
renzo9torres <201423485g@utea.edu.pe>
2022-10-11 12:21:06 -0500

    primer comit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.indigo,
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircleAvatar(
+              radius: 60.0,
+              backgroundColor: Colors.white,
+              backgroundImage: AssetImage("assets/images/persona1.jpeg"),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            const Text(
+              "Fiorela De Fátima Guadalupe",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 35.0,
+                fontWeight: FontWeight.w800,
+                fontFamily: "Dancing",
+                fontStyle: FontStyle.italic,
+              ),
+            ),
+            const Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                color: Colors.white38,
+                fontSize: 18.0,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 2.0,
+              ),
+            ),
+            const Divider(
+              thickness: 0.70,
+              color: Colors.white38,
+              indent: 120.0,
+              endIndent: 120.0,
+            ),
+            const SizedBox(
+              height: 10.0,
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("+51 966933559"),
+                subtitle: Text("Teléfono"),
+                leading: Icon(
+                  Icons.phone,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            const Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 15.0, vertical: 8.0),
+              elevation: 10.0,
+              child: ListTile(
+                title: Text("201423485g@utea1edu.pe"),
+                subtitle: Text("Correo electrónico"),
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.indigo,
+                ),
+                trailing: Icon(
+                  Icons.check_circle_outline,
+                  color: Colors.indigo,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 30.0,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Image.asset(
+                  "assets/images/facebook.png",
+                  height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+                Image.asset(
+                  "assets/images/instagram.png",
+                  height: 60.0,
+                ),
+                const SizedBox(
+                  width: 30.0,
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
